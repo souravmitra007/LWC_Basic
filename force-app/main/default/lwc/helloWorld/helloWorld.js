1ba3c1d471070d015a3785509c645a05864323f5
Sourav Mitra <you@example.com>
2021-05-02 23:55:35 +0530

    Updates made in the html and js codes HelloWorld

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,9 +3,18 @@ import { LightningElement,track } from 'lwc';
 export default class HelloWorld extends LightningElement {
    
     @track greeting = 'World';
-
+    @track temp;
+    
     changeHandler(event){
         this.greeting = event.target.value;
     }
+
+    changeEvent(event){
+        var inputCmp = this.template.querySelector(".inputCmp");
+        var val ='Hello' + ' ' + inputCmp.value;
+      this.temp = val;
+    }
+
+
 
 }
